212bf0ad63beb44adcde3e5795776f4f1f48c562
LukasCml <lukas.camehl@gmx.de>
2020-11-06 19:51:32 +0100

    Version 2

--- _bungen_For-Schleifen/_bungen_For-Schleifen.ino
+++ _bungen_For-Schleifen/_bungen_For-Schleifen.ino
@@ -1,54 +1,66 @@
 /*
-  void setup()
-  {
-  Serial.begin(9600);
-  }
+   -5 -2 1 4 7 10 13 16 19
+*/
 
-  void loop()
-  {
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
   for (int i = -5; i < 20; i = i + 3)
   {
     Serial.println(i);
     delay(500);
   }
-  }
+}
 
+************************************************
+/*
+   1 2 4 8 16 32 64 128 256 512 1024 2048 4096
 */
 
-/*
-  void setup()
-  {
+void setup()
+{
   Serial.begin(9600);
-  }
+}
 
-  void loop()
-  {
+void loop()
+{
   for (int i = 1; i < 4097; i = i * 2)
   {
     Serial.println(i);
     delay(5000);
   }
-  }
+}
+
+***********************************************
+/*
+  3 4 6 10 18 34 66 130 258 514 1026 2050 4098
 */
 
 
-/*
-  int z = 2;
+int z = 2;
 
-  void setup()
-  {
+void setup()
+{
   Serial.begin(9600);
-  }
+}
 
-  void loop()
-  {
+void loop()
+{
   for (int i = 3; i < 4099 ; i = i * 2 - z )
   {
     Serial.println(i);
     delay(500);
   }
   z = 2;
-  }
+}
+
+**********************************************
+/*
+  1 2 4 7 11 16 22 29 37 46 56 67 79 92
 */
 
 
